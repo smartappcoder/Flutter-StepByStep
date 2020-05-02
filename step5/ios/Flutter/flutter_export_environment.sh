1e9bfefa66f107647a8bcee9397dac2615d0fb83
bidatacloud <dennis.sheu@gmail.com>
2020-05-01 22:36:24 -0700

    Tested on iOS.

--- step5/ios/Flutter/flutter_export_environment.sh
+++ step5/ios/Flutter/flutter_export_environment.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/home/dennis/Toolchains/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/dennis/groomlake/github/Flutter-StepByStep/step5"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_ROOT=/Users/bidev01/GroomLake/Google/flutter/github-src/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/bidev01/GroomLake/github/Flutter-StepByStep/step5"
+export "FLUTTER_TARGET=/Users/bidev01/GroomLake/github/Flutter-StepByStep/step5/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
-export "FLUTTER_FRAMEWORK_DIR=/home/dennis/Toolchains/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/bidev01/GroomLake/Google/flutter/github-src/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
+export "TRACK_WIDGET_CREATION=true"
